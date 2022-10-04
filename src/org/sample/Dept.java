d8c71725289d677716c2a96c387ccea8c04f91dd
SARAVANANRUDHVIK <saravana26391@gmail.com>
2022-10-04 22:59:06 +0530

    rudhvik add a code

--- src/org/sample/Dept.java
+++ src/org/sample/Dept.java
@@ -3,6 +3,7 @@ package org.sample;
 public class Dept{
 	public void deptname () {
 		System.out.println("civil");
+		System.out.println("mech");
 	}
 	public static void main(String[] args) {
 		Dept a= new Dept();
